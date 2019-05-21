1d3c4c07f763f76f47c1fb059ccfc92647256e07
sax <s.apostolico@gmail.com>
2019-05-21 23:53:14 +0200

    add ProxyFix support

--- src/birder/web/app.py
+++ src/birder/web/app.py
@@ -5,6 +5,7 @@ from flask.logging import default_handler
 from flask_bootstrap import Bootstrap
 from flask_caching import Cache
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from ..config import Config
 
@@ -20,6 +21,7 @@ app.config.from_object('birder.config.Config')
 bootstrap = Bootstrap(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 CORS(app, supports_credentials=True, origins=app.config['CORS_ALLOW_ORIGIN'])
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 from . import views  # isort:skip
 from . import template_utils  # noqa isort:skip
