e2146cdaf7bc6784be808e296be371685c660f09
hanarero <hanarero@gmail.com>
2025-05-24 22:26:26 +0200

    updates

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 
 export MEDIA_ROOT="${MEDIA_ROOT:-/app/media}"
 export STATIC_ROOT="${STATIC_ROOT:-/app/static/}"
-export UWSGI_PROCESSES="${UWSGI_PROCESSES:-"4"}"
-export DJANGO_SETTINGS_MODULE="birder.config.settings"
+#export UWSGI_PROCESSES="${UWSGI_PROCESSES:-"4"}"
+#export DJANGO_SETTINGS_MODULE="birder.config.settings"
 
 export START_APP="${START_APP:-True}"
 export START_WORKER="${START_WORKER:-True}"
@@ -12,8 +12,6 @@ export START_CRON="${START_CRON:-True}"
 
 mkdir -p $MEDIA_ROOT $STATIC_ROOT /var/lib/nginx
 chown -R brd:os4d /app /var/lib/nginx
-
-echo 111111
 
 case "$1" in
     run)
