775985730aa331ad7b2d035277422411b4fc182f
hanarero <hanarero@gmail.com>
2025-05-27 21:33:16 +0200

    fixes tests

--- tests/extras/testutils/factories/project.py
+++ tests/extras/testutils/factories/project.py
@@ -6,6 +6,8 @@ from .base import AutoRegisterModelFactory
 
 
 class ProjectFactory(AutoRegisterModelFactory):
+    name = factory.Sequence(lambda n: f"Project-{n}")
+
     class Meta:
         model = Project
 
@@ -20,6 +22,8 @@ class ProjectFactory(AutoRegisterModelFactory):
 
 
 class EnvironmentFactory(AutoRegisterModelFactory):
+    name = factory.Sequence(lambda n: f"Environment-{n}")
+
     class Meta:
         model = Environment
         django_get_or_create = ["name"]
