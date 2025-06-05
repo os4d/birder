e06c298fa62017e3077ca8d03648ee901be772f7
hanarero <hanarero@gmail.com>
2025-06-05 14:09:18 +0200

    upgrade python

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -3,15 +3,9 @@
 
 export MEDIA_ROOT="${MEDIA_ROOT:-/app/media}"
 export STATIC_ROOT="${STATIC_ROOT:-/app/static/}"
-#export UWSGI_PROCESSES="${UWSGI_PROCESSES:-"4"}"
-#export DJANGO_SETTINGS_MODULE="birder.config.settings"
-
-export START_APP="${START_APP:-True}"
-export START_WORKER="${START_WORKER:-True}"
-export START_CRON="${START_CRON:-True}"
 
 mkdir -p $MEDIA_ROOT $STATIC_ROOT /var/lib/nginx
-chown -R brd:os4d /app /var/lib/nginx
+chown -R birder:os4d /app /var/lib/nginx
 
 echo "Command line is: '$1'"
 
