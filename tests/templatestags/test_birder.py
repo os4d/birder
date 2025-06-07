ab5eeef0087bec0d43bc649194a8804eda57728b
hanarero <hanarero@gmail.com>
2025-06-07 07:12:50 +0200

    add tests

--- tests/templatestags/test_birder.py
+++ tests/templatestags/test_birder.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+import freezegun
+from constance.test import override_config
 from django.template import Context, Template
 
-from birder.templatetags.birder import number, status
+from birder.templatetags.birder import format_date, number, status
 
 
 def test_status():
@@ -16,7 +20,13 @@ def test_number():
 
 
 def test_absolute_url(rf):
-    """load birder {% absolute_url aaa %}"""
     req = rf.get("/")
     tpl = Template("{% load birder %}{% absolute_url 'index' %}")
     assert tpl.render(Context({"request": req, "aaa": "/"}))
+
+
+@override_config(DATETIME_FORMAT="%Y %B %d - %H:%M")
+def test_format_date(db):
+    assert format_date(None) == "-"
+    with freezegun.freeze_time("2020-12-31 09:10:05"):
+        assert format_date(datetime.now()) == "2020 December 31 - 09:10"
