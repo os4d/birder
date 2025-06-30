725858587dd75ddb759053f414754dcd961fca4a
hanarero <hanarero@gmail.com>
2025-06-30 15:23:00 +0200

    fix: Resolve linting issues and add channels_redis cleanup (#39)
    * fix: Resolve linting issues and add channels_redis cleanup

---
+++ tests/ws/test_consumers.py
@@ -0,0 +1,31 @@
+import os
+from unittest import mock
+
+import pytest
+from channels.testing import WebsocketCommunicator
+
+from birder.ws.consumers import CheckConsumer
+
+
+@pytest.mark.skipif(os.environ.get("CI") == "true", reason="Skipping async tests on CI")
+@pytest.mark.asyncio
+async def test_check_consumer_connect():
+    communicator = WebsocketCommunicator(CheckConsumer.as_asgi(), "/ws/check/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+
+    response = await communicator.receive_json_from()
+    assert response == {"type": "connect"}
+
+    await communicator.disconnect()
+
+
+@pytest.mark.skipif(os.environ.get("CI") == "true", reason="Skipping async tests on CI")
+@pytest.mark.asyncio
+async def test_check_consumer_disconnect():
+    communicator = WebsocketCommunicator(CheckConsumer.as_asgi(), "/ws/check/")
+    await communicator.connect()
+
+    with mock.patch("birder.ws.consumers.async_to_sync") as mock_async_to_sync:
+        await communicator.disconnect()
+        mock_async_to_sync.assert_called_once()
